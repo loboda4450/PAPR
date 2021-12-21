52078f06fb820262fa8559469ef997e16fc7047d
loboda4450 <loboda4450@gmail.com>
2021-12-21 18:23:22 +0100

    PP03 done

--- PP03/xSepia.cpp
+++ PP03/xSepia.cpp
@@ -104,7 +104,7 @@ void xSepia_STD::testYUVtoRGBtoYUV_FLT(xPic &Dst, const xPic &Src) {
     }
 }
 
-void xSepia_STD::testYUVtoRGBtoYUV_INT(xPic &Dst, const xPic &Src) { // idk sth weird happens to colors..
+void xSepia_STD::testYUVtoRGBtoYUV_INT(xPic &Dst, const xPic &Src) {
     assert(Dst.getWidth() == Src.getWidth() && Dst.getHeight() == Src.getHeight() &&
            Dst.getStride() == Src.getStride());
     const int32 Width = Dst.getWidth();
@@ -214,7 +214,6 @@ void xSepia_STD::applySepiaEffect_FLT(xPic &Dst, const xPic &Src) {
                             0
                     };
 
-            //apply sepia effect
             const flt32V4 DstPixelRGB_F32 =
                     {
                             SrcPixelRGB_F32[0] * SEPIA_F32[0][0] + SrcPixelRGB_F32[1] * SEPIA_F32[0][1] +
@@ -227,15 +226,16 @@ void xSepia_STD::applySepiaEffect_FLT(xPic &Dst, const xPic &Src) {
                     };
 
             //convert RGB --> YCbCr (BT.709)
-            const flt32V4 DstPixelYUV_F32 = {
-                    DstPixelRGB_F32[0] * RGB2YCbCr_I32[0][0] + DstPixelRGB_F32[1] * RGB2YCbCr_I32[0][1] +
-                    DstPixelRGB_F32[2] * RGB2YCbCr_I32[0][2],
-                    DstPixelRGB_F32[0] * RGB2YCbCr_I32[1][0] + DstPixelRGB_F32[1] * RGB2YCbCr_I32[1][1] +
-                    DstPixelRGB_F32[2] * RGB2YCbCr_I32[1][2],
-                    DstPixelRGB_F32[0] * RGB2YCbCr_I32[2][0] + DstPixelRGB_F32[1] * RGB2YCbCr_I32[2][1] +
-                    DstPixelRGB_F32[2] * RGB2YCbCr_I32[2][2],
-                    0
-            };
+            const flt32V4 DstPixelYUV_F32 =
+                    {
+                            DstPixelRGB_F32[0] * RGB2YCbCr_F32[0][0] + DstPixelRGB_F32[1] * RGB2YCbCr_F32[0][1] +
+                            DstPixelRGB_F32[2] * RGB2YCbCr_F32[0][2],
+                            DstPixelRGB_F32[0] * RGB2YCbCr_F32[1][0] + DstPixelRGB_F32[1] * RGB2YCbCr_F32[1][1] +
+                            DstPixelRGB_F32[2] * RGB2YCbCr_F32[1][2],
+                            DstPixelRGB_F32[0] * RGB2YCbCr_F32[2][0] + DstPixelRGB_F32[1] * RGB2YCbCr_F32[2][1] +
+                            DstPixelRGB_F32[2] * RGB2YCbCr_F32[2][2],
+                            0
+                    };
 
             //change data format (and apply chroma offset) + clip to range 0-Max
             flt32V4 TmpPixelYUV_F32 = DstPixelYUV_F32 + flt32V4(0, DstMidValue, DstMidValue, 0);
@@ -254,14 +254,89 @@ void xSepia_STD::applySepiaEffect_FLT(xPic &Dst, const xPic &Src) {
         SrcPtrCb += SrcStride;
         SrcPtrCr += SrcStride;
     }
-
 }
 
 void xSepia_STD::applySepiaEffect_INT(xPic &Dst, const xPic &Src) {
+
     assert(Dst.getWidth() == Src.getWidth() && Dst.getHeight() == Src.getHeight() &&
            Dst.getStride() == Src.getStride());
-    //WRITE ME!
-    assert(0);
+    const int32 Width = Dst.getWidth();
+    const int32 Height = Dst.getHeight();
+    uint16 *restrict DstPtrLm = Dst.getAddr(0);
+    uint16 *restrict DstPtrCb = Dst.getAddr(1);
+    uint16 *restrict DstPtrCr = Dst.getAddr(2);
+    const uint16 *SrcPtrLm = Src.getAddr(0);
+    const uint16 *SrcPtrCb = Src.getAddr(1);
+    const uint16 *SrcPtrCr = Src.getAddr(2);
+    const int32 DstStride = Dst.getStride();
+    const int32 SrcStride = Src.getStride();
+
+    const int32 SrcMidValue = int32(1 << (Src.getBitDepth() - 1));
+    const int32 DstMidValue = int32(1 << (Dst.getBitDepth() - 1));
+    const int32 DstMaxValue = int32((1 << Dst.getBitDepth()) - 1);
+
+    for (int32 y = 0; y < Height; y++) {
+        for (int32 x = 0; x < Width; x++) {
+            //load
+            const uint16V4 SrcPixelYUV_U16 = {SrcPtrLm[x], SrcPtrCb[x], SrcPtrCr[x], 0};
+
+            //change data format (and remove chroma offset)
+            const int32V4 SrcPixelYUV_I32 = {
+                    (int32) (SrcPixelYUV_U16[0] << 16),
+                    (int32) (SrcPixelYUV_U16[1]) - SrcMidValue,
+                    (int32) (SrcPixelYUV_U16[2]) - SrcMidValue,
+                    0
+            };
+
+            //convert YCbCr --> RGB (BT.709)
+            const int32V4 SrcPixelRGB_I32 = {
+                    (SrcPixelYUV_I32[0] + SrcPixelYUV_I32[2] * YCbCr2RGB_I32[0][2] + 32768) >> 16,
+                    (SrcPixelYUV_I32[0] + SrcPixelYUV_I32[1] * YCbCr2RGB_I32[1][1] +
+                     SrcPixelYUV_I32[2] * YCbCr2RGB_I32[1][2] + 32768) >> 16,
+                    (SrcPixelYUV_I32[0] + SrcPixelYUV_I32[1] * YCbCr2RGB_I32[2][1] + 32768) >> 16,
+                    0
+            };
+
+            //copy RGB --> RGB
+            const int32V4 DstPixelRGB_I32 =
+                    {
+                            (SrcPixelRGB_I32[0] * SEPIA_I32[0][0] + SrcPixelRGB_I32[1] * SEPIA_I32[0][1] +
+                             SrcPixelRGB_I32[2] * SEPIA_I32[0][2] + 32768) >> 16,
+                            (SrcPixelRGB_I32[0] * SEPIA_I32[1][0] + SrcPixelRGB_I32[1] * SEPIA_I32[1][1] +
+                             SrcPixelRGB_I32[2] * SEPIA_I32[1][2] + 32768) >> 16,
+                            (SrcPixelRGB_I32[0] * SEPIA_I32[2][0] + SrcPixelRGB_I32[1] * SEPIA_I32[2][1] +
+                             SrcPixelRGB_I32[2] * SEPIA_I32[2][2] + 32768) >> 16,
+                            0
+                    };
+
+            //convert RGB --> YCbCr (BT.709)
+            const int32V4 DstPixelYUV_I32 = {
+                    (int32) (((DstPixelRGB_I32[0] * RGB2YCbCr_I32[0][0] + DstPixelRGB_I32[1] * RGB2YCbCr_I32[0][1] +
+                               DstPixelRGB_I32[2] * RGB2YCbCr_I32[0][2]) + 32768) >> 16) + 4,
+                    (int32) (((DstPixelRGB_I32[0] * RGB2YCbCr_I32[1][0] + DstPixelRGB_I32[1] * RGB2YCbCr_I32[1][1] +
+                               DstPixelRGB_I32[2] * RGB2YCbCr_I32[1][2]) + 32768) >> 16) - 2,
+                    (int32) (((DstPixelRGB_I32[0] * RGB2YCbCr_I32[2][0] + DstPixelRGB_I32[1] * RGB2YCbCr_I32[2][1] +
+                               DstPixelRGB_I32[2] * RGB2YCbCr_I32[2][2]) + 32768) >> 16) - 8,
+                    0
+            };
+
+            //change data format (and apply chroma offset) + clip to range 0-Max
+            int32V4 TmpPixelYUV_I32 = DstPixelYUV_I32 + int32V4(0, DstMidValue, DstMidValue, 0);
+            TmpPixelYUV_I32.clip(xMakeVec4<int32>(0), xMakeVec4<int32>(DstMaxValue));
+            const uint16V4 DstPixelYUV_U16 = uint16V4(TmpPixelYUV_I32);
+
+            //store
+            DstPtrLm[x] = DstPixelYUV_U16[0];
+            DstPtrCb[x] = DstPixelYUV_U16[1];
+            DstPtrCr[x] = DstPixelYUV_U16[2];
+        }
+        DstPtrLm += DstStride;
+        DstPtrCb += DstStride;
+        DstPtrCr += DstStride;
+        SrcPtrLm += SrcStride;
+        SrcPtrCb += SrcStride;
+        SrcPtrCr += SrcStride;
+    }
 }
 
 //===============================================================================================================================================================================================================
